91a865149b16920fd4a73a9aa8c092b470e26445
Swizzlr <git@swizzlr.co>
2017-01-10 12:23:34 +0000

    Pin minor versions.

--- Package.swift
+++ Package.swift
@@ -3,8 +3,8 @@ import PackageDescription
 let package = Package(
   name: "TitanNestAdapter",
   dependencies: [
-    .Package(url: "https://github.com/bermudadigitalstudio/titan-core.git", majorVersion: 0),
-    .Package(url: "https://github.com/nestproject/Nest.git", majorVersion: 0),
-    .Package(url: "https://github.com/nestproject/Inquiline.git", majorVersion: 0)
+    .Package(url: "https://github.com/bermudadigitalstudio/titan-core.git", majorVersion: 0, minor: 1),
+    .Package(url: "https://github.com/nestproject/Nest.git", majorVersion: 0, minor: 4),
+    .Package(url: "https://github.com/nestproject/Inquiline.git", majorVersion: 0, minor: 4)
   ]
 )
